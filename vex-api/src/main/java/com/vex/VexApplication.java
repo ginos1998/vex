392184f78eb1e899df9e5337e1cdf51c8bffe012
ginos1998 <gino.siampichetti@deepcreeksolutions.com>
2023-12-22 22:01:33 -0300

    first commit

---
+++ vex-api/src/main/java/com/vex/VexApplication.java
@@ -0,0 +1,13 @@
+package com.vex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VexApplication.class, args);
+	}
+
+}
---
+++ vex-api/src/test/java/com/vex/VexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
