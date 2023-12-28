6241426c88bec518a6cc0b248c90cf51e38f88d0
ginos1998 <gino.siampichetti@deepcreeksolutions.com>
2023-12-28 19:01:20 -0300

    starting auth-server

---
+++ vex-server-auth/src/main/java/com/vex/VexServerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.vex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VexServerAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VexServerAuthApplication.class, args);
+    }
+
+}
---
+++ vex-server-auth/src/test/java/com/vex/VexServerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VexServerAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
